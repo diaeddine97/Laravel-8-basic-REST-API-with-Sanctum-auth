c9cb4dacfbee394de2c007a9203c4633cb36211f
diaeddine <diaeddine97@gmail.com>
2021-04-09 01:48:15 +0100

    basic rest with sanctum auth

---
+++ routes/api.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Public routes
+Route::resource('products', ProductController::class)->only(['index', 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+
+//Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function(){
+
+    Route::resource('products', ProductController::class)->except(['index', 'show']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
+
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
